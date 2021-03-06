fded93960460a92912ec6ec1590f4cf8ae37c5a1
Fletcher Dick <fdi19@uclive.ac.nz>
2021-03-06 22:53:46 +1300

    Added the events routes, refactored existing route filenames

---
+++ app/routes/events.routes.js
@@ -0,0 +1,21 @@
+const events = require('../controllers/events.controller');
+
+module.exports = function (app) {
+    app.route(app.rootUrl + '/events')
+        .get(events.getEvents);
+
+    app.route(app.rootUrl + '/events')
+        .post(events.addEvent);
+
+    app.route(app.rootUrl + '/events/:id')
+        .get(events.getEvent);
+
+    app.route(app.rootUrl + '/events/:id')
+        .patch(events.updateEvent);
+
+    app.route(app.rootUrl + '/events/:id')
+        .delete(events.deleteEvent);
+
+    app.route(app.rootUrl + '/events/categories')
+        .get(events.getCategories);
+};
