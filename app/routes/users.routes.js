c29e2387eee824f906e8eb977bed228e7dea33d2
Fletcher Dick <fdi19@uclive.ac.nz>
2021-03-06 22:33:00 +1300

    Added the rest of the users routes

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -3,4 +3,16 @@ const users = require('../controllers/users.controller');
 module.exports = function (app) {
     app.route(app.rootUrl + '/users/register')
         .post(users.registerUser);
+
+    app.route(app.rootUrl + '/users/login')
+        .post(users.loginUser);
+
+    app.route(app.rootUrl + '/users/logout')
+        .post(users.logoutUser);
+
+    app.route(app.rootUrl + '/users/:id')
+        .get(users.getUser);
+
+    app.route(app.rootUrl + '/users/:id')
+        .patch(users.updateUser);
 };
