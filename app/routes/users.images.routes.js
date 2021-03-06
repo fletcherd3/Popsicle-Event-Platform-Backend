8c51fba972298d7c6e89d8cba2058c2f94111c66
Fletcher Dick <fdi19@uclive.ac.nz>
2021-03-06 22:36:51 +1300

    Added the users.images routes

---
+++ app/routes/users.images.routes.js
@@ -0,0 +1,12 @@
+const usersImages = require('../controllers/users.images.controller');
+
+module.exports = function (app) {
+    app.route(app.rootUrl + '/users/:id/image')
+        .get(usersImages.getUserImage);
+
+    app.route(app.rootUrl + '/users/:id/image')
+        .put(usersImages.setUserImage);
+
+    app.route(app.rootUrl + '/users/:id/image')
+        .delete(usersImages.deleteUserImage);
+};
